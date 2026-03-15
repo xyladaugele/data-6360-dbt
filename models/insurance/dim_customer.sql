830d7de0aa06c734bb7a71342ed10a54bd1a1219
Xyla Daugele <a02382825@usu.edu>
2026-03-15 17:31:13 +0000

    finished exercise and checked that data exists and is correct

--- models/insurance/dim_customer.sql
+++ models/insurance/dim_customer.sql
@@ -4,15 +4,44 @@
     )
 }}
 
+with db_source as (
+    select
+    customerid,
+    firstname,
+    lastname,
+    dob,
+    address,
+    city,
+    state,
+    zipcode
+    FROM {{ source('insurance_landing', 'customers') }}
+)
 
-select
-customerid as customer_key,
-customerid,
-firstname,
-lastname,
-dob,
-address,
-city,
-state,
-zipcode
-FROM {{ source('insurance_landing', 'customers') }}
+, cs_interactions_source as (
+    select distinct 
+        customer_first_name,
+        customer_last_name,
+        customer_email
+    from {{ ref('stg_customer_service_interactions')}}
+)
+, final as (
+    select 
+    d.customerid,
+    coalesce(d.firstname, cs.customer_first_name) as firstname,
+    coalesce(d.lastname, cs.customer_last_name) as lastname,
+    d.dob,
+    d.address,
+    d.city,
+    d.state,
+    d.zipcode,
+    cs.customer_email
+    from db_source d
+    full join cs_interactions_source cs
+    on d.firstname = cs.customer_first_name
+    and d.lastname = cs.customer_last_name
+)
+
+select 
+    {{ dbt_utils.generate_surrogate_key(['firstname', 'lastname'])}} as customer_key,
+    *
+from final
